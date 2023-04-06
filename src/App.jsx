926d67fb036cba375a578a728cbadf1d76b2d2a1
abdus251 <abdus251@gmail.com>
2023-04-06 16:34:32 +0600

    add cart component and calculation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
+import Shop from './components/Shop/Shop'
+import Product from './components/Products/Product'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,8 @@ function App() {
   return (
     <div className="App">
         <Header></Header>
+        <Shop></Shop>
+        {/* <Product></Product> */}
     </div>
   )
 }
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Products/Product';
+import './Shop.css'
+import Cart from '../Cart/Cart';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
+    
+    useEffect( () =>{
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+ }, []);
+
+ const handleAddToCart = (product) => {
+    // cart.push(product);
+    const newCart = [...cart, product];
+    setCart(newCart);
+    console.log(newCart,"newCart")
+}
+    
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+            {
+                products.map(product => <Product 
+                    key={product.id}
+                    product={product}
+                    handleAddToCart={handleAddToCart}
+            ></Product>)
+            }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
